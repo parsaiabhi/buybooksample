d41e20222b2739a18e93d69dbf519b797accf044
abhishek <tech.parsai@gmail.com>
2022-06-30 11:21:27 +0530

    commit

---
+++ src/main/java/com/book/buybook/BuybookApplication.java
@@ -0,0 +1,13 @@
+package com.book.buybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/buybook/BuybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.buybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
